b53044b59340b0076d7038e7afb61b0fe1c13b20
7malahit7 <60061637+7malahit7@users.noreply.github.com>
2021-06-16 21:25:00 +0300

    Add files via upload

--- Tetris.cpp
+++ Tetris.cpp
@@ -19,14 +19,15 @@ int main()
 
 	while (true)
 	{
-		int I1{ 0 }, J1{ 0 }, I2{ 0 }, J2{ 0 }, I3{ 0 }, J3{ 0 }, I4{ 0 }, J4{ 0 };
+		int I1{ 0 }, J1{ 0 }, I2{ 0 }, J2{ 0 }, I3{ 0 }, J3{ 0 }, I4{ 0 }, J4{ 0 }, Rand = rand() % 7, pos{ 1 }, swap{ 1 }, slp{ 0 }, sleep{ 0 }, points{ 0 }, levelpoints{ 100 }, level{ 1 }, speedcontrol{ 0 };
 		bool GameOver{ false };
 
-		Figure(field, I1, J1, I2, J2, I3, J3, I4, J4, GameOver);
+		Figure(field, I1, J1, I2, J2, I3, J3, I4, J4, Rand, GameOver);
 		Print(field);
 
 		if (GameOver == true)
 		{
+			Sleep(500);
 			system("cls");
 			break;
 		}
@@ -34,43 +35,20 @@ int main()
 
 		while (I1 < 19 && I2 < 19 && I3 < 19 && I4 < 19)
 		{
-			if (field[I1 + 1][J1] == '@' && I1 + 1 != I2 && I1 + 1 != I3 && I1 + 1 != I4 && J1 != J2 && J1 != J3 && J1 != J4 || field[I2 + 1][J2] == '@' && I2 + 1 != I1 && I2 + 1 != I3 && I2 + 1 != I4 && J2 != I1 && J2 != J3 && J2 != J4 || field[I3 + 1][J3] == '@' && I3 + 1 != I1 && I3 + 1 != I2 && I3 + 1 != I4 && J3 != J1 && J3 != J2 && J3 != J4 || field[I4 + 1][J4] == '@' && I4 + 1 != I1 && I4 + 1 != I2 && I4 + 1 != I3 && J4 != J1 && J4 != J2 && J4 != J3) break;
-			
-			int slp{ 0 };
-			char WASDE = ' ';
-			if (_kbhit())
+			for (int i = 0; i < 20; i++)
 			{
-				WASDE = _getch();
-				if (WASDE == 'a' || WASDE == 'A' || WASDE == 'ô' || WASDE == 'Ô')
-				{
-					bool doleft{ true };
-					if (field[I1][J1 - 1] == '@' && J1 - 1 != J2 && J1 - 1 != J3 && J1 - 1 != J4 || field[I2][J2 - 1] == '@' && J2 - 1 != I1 && J2 - 1 != J3 && J2 - 1 != J4 || field[I3][J3 - 1] == '@' && J3 - 1 != J1 && J3 - 1 != J2 && J3 - 1 != J4 || field[I4][J4 - 1] == '@' && J4 - 1 != J1 && J4 - 1 != J2 && J4 - 1 != J3 || J1 == 0 || J2 == 0 || J3 == 0 || J4 == 0) doleft = false;
-					if (doleft == true)
-					{
-						Left(field, I1, J1, I2, J2, I3, J3, I4, J4);
-					}
-				}
-
-				if (WASDE == 's' || WASDE == 'S' || WASDE == 'û' || WASDE == 'Û')
-				{
-					slp = 490;
-				}
-
-				if (WASDE == 'd' || WASDE == 'D' || WASDE == 'â' || WASDE == 'Â')
-				{
-					bool doright{ true };
-					if (field[I1][J1 + 1] == '@' && J1 + 1 != J2 && J1 + 1 != J3 && J1 + 1 != J4 || field[I2][J2 + 1] == '@' && J2 + 1 != I1 && J2 + 1 != J3 && J2 + 1 != J4 || field[I3][J3 + 1] == '@' && J3 + 1 != J1 && J3 + 1 != J2 && J3 + 1 != J4 || field[I4][J4 + 1] == '@' && J4 + 1 != J1 && J4 + 1 != J2 && J4 + 1 != J3 || J1 == 9 || J2 == 9 || J3 == 9 || J4 == 9) doright = false;
-					if (doright == true)
-					{
-						Right(field, I1, J1, I2, J2, I3, J3, I4, J4);
-					}
-				}
+				Sleep(100 - slp);
+				WASDE(field, I1, J1, I2, J2, I3, J3, I4, J4, slp, Rand, pos, swap, GameOver);
 			}
 
-			if (field[I1 + 1][J1] == '@' && I1 + 1 != I2 && I1 + 1 != I3 && I1 + 1 != I4 && J1 != J2 && J1 != J3 && J1 != J4 || field[I2 + 1][J2] == '@' && I2 + 1 != I1 && I2 + 1 != I3 && I2 + 1 != I4 && J2 != I1 && J2 != J3 && J2 != J4 || field[I3 + 1][J3] == '@' && I3 + 1 != I1 && I3 + 1 != I2 && I3 + 1 != I4 && J3 != J1 && J3 != J2 && J3 != J4 || field[I4 + 1][J4] == '@' && I4 + 1 != I1 && I4 + 1 != I2 && I4 + 1 != I3 && J4 != J1 && J4 != J2 && J4 != J3) break;
+			bool BREAK{ false };
+			Check(field, I1, J1, I2, J2, I3, J3, I4, J4,Rand, pos, BREAK);
+			if (BREAK == true) break;
 
 			Down(field, I1, J1, I2, J2, I3, J3, I4, J4);
-			Sleep(500 - slp);
+
+			Check(field, I1, J1, I2, J2, I3, J3, I4, J4, Rand, pos, BREAK);
+			if (BREAK == true) break;
 		}
 		system("cls");
 	}
--- functions.h
+++ functions.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <conio.h>
 #include <time.h>
 #include <windows.h>
 
@@ -120,17 +121,15 @@ void I(char** field)
 	field[3][4] = '@';
 }
 
-int Figure(char** field, int &I1, int &J1, int &I2, int &J2, int &I3, int &J3, int &I4, int &J4, bool &GameOver)
+int Figure(char** field, int& I1, int& J1, int& I2, int& J2, int& I3, int& J3, int& I4, int& J4, int& Rand, bool& GameOver)
 {
-	int Rand = rand() % 7;
-
 	switch (Rand)
 	{
 	case 0:
 	{
 		I1 = 0; J1 = 4;
 		I2 = 0; J2 = 5;
-		I3 = 1; J3 = 3; 
+		I3 = 1; J3 = 3;
 		I4 = 1; J4 = 4;
 
 		if (field[I1][J1] == '@' || field[I2][J2] == '@' || field[I3][J3] == '@' || field[I4][J4] == '@') GameOver = true;
@@ -145,7 +144,7 @@ int Figure(char** field, int &I1, int &J1, int &I2, int &J2, int &I3, int &J3, i
 		I1 = 0; J1 = 3;
 		I2 = 0; J2 = 4;
 		I3 = 1; J3 = 4;
-		I4 = 1; J4 = 5; 
+		I4 = 1; J4 = 5;
 
 		if (field[I1][J1] == '@' || field[I2][J2] == '@' || field[I3][J3] == '@' || field[I4][J4] == '@') GameOver = true;
 
@@ -154,7 +153,7 @@ int Figure(char** field, int &I1, int &J1, int &I2, int &J2, int &I3, int &J3, i
 		break;
 	}
 
-	case 2: 
+	case 2:
 	{
 		I1 = 0; J1 = 3;
 		I2 = 0; J2 = 4;
@@ -181,8 +180,8 @@ int Figure(char** field, int &I1, int &J1, int &I2, int &J2, int &I3, int &J3, i
 
 		break;
 	}
-		
-	case 4: 
+
+	case 4:
 	{
 		I1 = 0; J1 = 4;
 		I2 = 1; J2 = 4;
@@ -209,7 +208,7 @@ int Figure(char** field, int &I1, int &J1, int &I2, int &J2, int &I3, int &J3, i
 
 		break;
 	}
-		
+
 	case 6:
 	{
 		I1 = 0; J1 = 4;
@@ -226,7 +225,7 @@ int Figure(char** field, int &I1, int &J1, int &I2, int &J2, int &I3, int &J3, i
 
 	}
 
-	return I1, J1, I2, J2, I3, J3, I4, J4, GameOver;
+	return I1, J1, I2, J2, I3, J3, I4, J4, Rand, GameOver;
 }
 
 int Left(char** field, int& I1, int& J1, int& I2, int& J2, int& I3, int& J3, int& I4, int& J4)
@@ -287,4 +286,291 @@ int Right(char** field, int& I1, int& J1, int& I2, int& J2, int& I3, int& J3, in
 	Print(field);
 
 	return I1, J1, I2, J2, I3, J3, I4, J4;
+}
+
+int WASDE(char** field, int& I1, int& J1, int& I2, int& J2, int& I3, int& J3, int& I4, int& J4, int& slp, int& Rand, int& pos, int& swap, bool& GameOver)
+{
+	slp = 0;
+	char WASDE = ' ';
+	if (_kbhit())
+	{
+		WASDE = _getch();
+
+		if (WASDE == 'w' && I1 < 2 && swap == 1 || WASDE == 'W' && I1 < 2 && swap == 1 || WASDE == 'ö' && I1 < 2 && swap == 1 || WASDE == 'Ö' && I1 < 2 && swap == 1)
+		{
+			field[I1][J1] = '*';
+			field[I2][J2] = '*';
+			field[I3][J3] = '*';
+			field[I4][J4] = '*';
+
+			Rand = rand() % 7;
+
+			Figure(field, I1, J1, I2, J2, I3, J3, I4, J4, Rand, GameOver);
+			system("cls");
+			Print(field);
+
+			--swap;
+		}
+
+		if (WASDE == 'a' || WASDE == 'A' || WASDE == 'ô' || WASDE == 'Ô')
+		{
+			bool doleft{ true };
+			if (field[I1][J1 - 1] == '@' && J1 - 1 != J2 && J1 - 1 != J3 && J1 - 1 != J4 || field[I2][J2 - 1] == '@' && J2 - 1 != I1 && J2 - 1 != J3 && J2 - 1 != J4 || field[I3][J3 - 1] == '@' && J3 - 1 != J1 && J3 - 1 != J2 && J3 - 1 != J4 || field[I4][J4 - 1] == '@' && J4 - 1 != J1 && J4 - 1 != J2 && J4 - 1 != J3 || J1 == 0 || J2 == 0 || J3 == 0 || J4 == 0) doleft = false;
+			if (doleft == true)
+			{
+				Left(field, I1, J1, I2, J2, I3, J3, I4, J4);
+			}
+		}
+
+		if (WASDE == 's' || WASDE == 'S' || WASDE == 'û' || WASDE == 'Û')
+		{
+			slp = 99;
+		}
+
+		if (WASDE == 'd' || WASDE == 'D' || WASDE == 'â' || WASDE == 'Â')
+		{
+			bool doright{ true };
+			if (field[I1][J1 + 1] == '@' && J1 + 1 != J2 && J1 + 1 != J3 && J1 + 1 != J4 || field[I2][J2 + 1] == '@' && J2 + 1 != I1 && J2 + 1 != J3 && J2 + 1 != J4 || field[I3][J3 + 1] == '@' && J3 + 1 != J1 && J3 + 1 != J2 && J3 + 1 != J4 || field[I4][J4 + 1] == '@' && J4 + 1 != J1 && J4 + 1 != J2 && J4 + 1 != J3 || J1 == 9 || J2 == 9 || J3 == 9 || J4 == 9) doright = false;
+			if (doright == true)
+			{
+				Right(field, I1, J1, I2, J2, I3, J3, I4, J4);
+			}
+		}
+
+		if (WASDE == 'e' || WASDE == 'E' || WASDE == 'ó' || WASDE == 'Ó')
+		{
+			switch (Rand)
+			{
+			case 5: break;
+
+			case 0:
+			{
+				switch (pos)
+				{
+				case 1:
+				{
+					field[I1][J1] = '*';
+					field[I2][J2] = '*';
+					field[I3][J3] = '*';
+					field[I4][J4] = '*';
+
+					I1 = 1; J1 = 4;
+					I2 = 2; J2 = 4;
+					I3 = 0; J3 = 3;
+					I4 = 1; J4 = 3;
+
+					field[I1][J1] = '@';
+					field[I2][J2] = '@';
+					field[I3][J3] = '@';
+					field[I4][J4] = '@';
+
+					system("cls");
+					Print(field);
+
+					++pos;
+
+					break;
+				}
+
+				case 2:
+				{
+					field[I1][J1] = '*';
+					field[I2][J2] = '*';
+					field[I3][J3] = '*';
+					field[I4][J4] = '*';
+
+					I1 = 0; J1 = 4;
+					I2 = 0; J2 = 5;
+					I3 = 1; J3 = 3;
+					I4 = 1; J4 = 4;
+
+					field[I1][J1] = '@';
+					field[I2][J2] = '@';
+					field[I3][J3] = '@';
+					field[I4][J4] = '@';
+
+					system("cls");
+					Print(field);
+
+					--pos;
+
+					break;
+				}
+
+				}
+			}
+
+			case 1:
+			{
+				switch (pos)
+				{
+				case 1:
+				{
+					field[I1][J1] = '*';
+					field[I2][J2] = '*';
+					field[I3][J3] = '*';
+					field[I4][J4] = '*';
+
+					I1 = 0; J1 = 5;
+					I2 = 1; J2 = 5;
+					I3 = 1; J3 = 4;
+					I4 = 2; J4 = 4;
+
+					field[I1][J1] = '@';
+					field[I2][J2] = '@';
+					field[I3][J3] = '@';
+					field[I4][J4] = '@';
+
+					system("cls");
+					Print(field);
+
+					++pos;
+
+					break;
+				}
+
+				case 2:
+				{
+					field[I1][J1] = '*';
+					field[I2][J2] = '*';
+					field[I3][J3] = '*';
+					field[I4][J4] = '*';
+
+					I1 = 0; J1 = 3;
+					I2 = 0; J2 = 4;
+					I3 = 1; J3 = 4;
+					I4 = 1; J4 = 5;
+
+					field[I1][J1] = '@';
+					field[I2][J2] = '@';
+					field[I3][J3] = '@';
+					field[I4][J4] = '@';
+
+					system("cls");
+					Print(field);
+
+					--pos;
+
+					break;
+				}
+
+				}
+			}
+
+			case 2:
+			{
+				switch (pos)
+				{
+				case 1:
+				{
+					field[I1][J1] = '*';
+					field[I2][J2] = '*';
+					field[I3][J3] = '*';
+					field[I4][J4] = '*';
+
+					I1 = 0; J1 = 5;
+					I2 = 1; J2 = 5;
+					I3 = 2; J3 = 5;
+					I4 = 1; J4 = 4;
+
+					field[I1][J1] = '@';
+					field[I2][J2] = '@';
+					field[I3][J3] = '@';
+					field[I4][J4] = '@';
+
+					system("cls");
+					Print(field);
+
+					++pos;
+
+					break;
+				}
+
+				}
+			}
+
+
+
+			}
+		}
+	}
+
+	return I1, J1, I2, J2, I3, J3, I4, J4, slp, pos, swap;
+}
+
+int Check(char** field, int I1, int J1, int I2, int J2, int I3, int J3, int I4, int J4, int Rand, int pos, bool& BREAK)
+{
+	if (I1 < 19 && I2 < 19 && I3 < 19 && I4 < 19)
+	{
+		switch (Rand)
+		{
+		case 0:
+		{
+			switch (pos)
+			{
+			case 1: { if (field[I2 + 1][J2] == '@' || field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 2: { if (field[I4 + 1][J4] == '@' || field[I2 + 1][J2] == '@') BREAK = true; break; }
+			}
+		}
+
+		case 1:
+		{
+			switch (pos)
+			{
+			case 1: { if (field[I1 + 1][J1] == '@' || field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 2: { if (field[I2 + 1][J2] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			}
+		}
+
+		case 2:
+		{
+			switch (pos)
+			{
+			case 1: { if (field[I1 + 1][J1] == '@' || field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 2: { if (field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 3: { if (field[I1 + 1][J1] == '@' || field[I2 + 1][J2] == '@' || field[I3 + 1][J3] == '@') BREAK = true; break; }
+			case 4: { if (field[I1 + 1][J1] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			}
+		}
+
+		case 3:
+		{
+			switch (pos)
+			{
+			case 1: { if (field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 2: { if (field[I1 + 1][J1] == '@' || field[I2 + 1][J2] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 3: { if (field[I1 + 1][J1] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 4: { if (field[I1 + 1][J1] == '@' || field[I2 + 1][J2] == '@' || field[I3 + 1][J3] == '@') BREAK = true; break; }
+			}
+		}
+
+		case 4:
+		{
+			switch (pos)
+			{
+			case 1: { if (field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 2: { if (field[I1 + 1][J1] == '@' || field[I2 + 1][J2] == '@' || field[I3 + 1][J3] == '@') BREAK = true; break; }
+			case 3: { if (field[I1 + 1][J1] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 4: { if (field[I1 + 1][J1] == '@' || field[I2 + 1][J2] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			}
+		}
+
+		case 5:
+		{
+			if (field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break;
+		}
+
+		case 6:
+		{
+			switch (pos)
+			{
+			case 1: { if (field[I4 + 1][J4] == '@') BREAK = true; break; }
+			case 2: { if (field[I1 + 1][J1] == '@' || field[I2 + 1][J2] == '@' || field[I3 + 1][J3] == '@' || field[I4 + 1][J4] == '@') BREAK = true; break; }
+			}
+		}
+
+		}
+	}
+
+	return BREAK;
 }
